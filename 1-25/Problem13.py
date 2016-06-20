c79954ba46b308a304a8a56273a9e156b792e7f8
Oran Can Ören <orancanoren@gmail.com>
2016-06-20 16:24:37 +0300

    Update Problem13.py

--- 1-25/Problem13.py
+++ 1-25/Problem13.py
@@ -1,3 +1,7 @@
+#ProjectEuler problem 13
+#Solved: 11 May 2016
+#NOTE: For this code to work, a text file named '13.txt' containing 50 numbers provided in the problem is required in the same directory as this py file
+
 def getNumber():
     fo = open('13.txt','r')
     numbers = []
